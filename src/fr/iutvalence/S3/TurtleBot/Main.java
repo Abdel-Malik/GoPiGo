611ab150bdd0e3cfb5dc95eb139a0226738366a0
laetitiabourdon <laetitia-b-38@hotmail.fr>
2016-01-14 11:26:27 +0100

    Modif Main

--- src/fr/iutvalence/S3/TurtleBot/Main.java
+++ src/fr/iutvalence/S3/TurtleBot/Main.java
@@ -7,15 +7,15 @@ import javax.swing.JFrame;
 
 public class Main 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		//Communication_wifi communicationWifi;
 		Application application;
-			/*application = new Application(new Communication_wifi("192.168.1.157", 8080));
+			application = new Application(new Communication_wifi("192.168.1.157", 8080));
 			application.etablirConnexion();
 			application.envoyerDonnees();
 			application.lireDonneesServeur();
-			application.terminerConnexion();*/
+			application.terminerConnexion();
 			
 		}
 }

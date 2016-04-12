1100ceb5285dab8dc6c2a9f62855d9b0f81e33de
bouhassa <bouhassa@ES1256.esisar.inpg.fr>
2016-04-12 13:03:57 +0200

    suppression 2e envoi commande

--- src/fr/iutvalence/S3/TurtleBot/Application.java
+++ src/fr/iutvalence/S3/TurtleBot/Application.java
@@ -48,8 +48,13 @@ public class Application {
 		while (true)
 		{
 			String choix = this.interfaceEntree.demandeAction();
-				choix += '\0';
 				this.envoyerDonnees(choix);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				lireDonneesServeur();
 		}
 	}
